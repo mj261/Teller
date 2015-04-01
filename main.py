eb57a2d58302b06d3f3fb66535aadc41c8fdda35
Matthew Jones <matthewjones1170@gmail.com>
2015-04-01 18:03:12 -0500

    Welcome screen with options

--- main.py
+++ main.py
@@ -2,8 +2,31 @@ __author__ = 'Matthew Jones', 'Daniel Downs', 'Jennifer Jozefiak', 'Grant Richar
 
 import conn_db
 
-conn = conn_db.connect()
+##conn = conn_db.connect()
 
+print("#####################################################")
+print("###                                               ###")
+print("###              Welcome to our Bank              ###")
+print("###                                               ###")
+print("###             Downs, Jones, Jozefiak            ###")
+print("###             Richards, & Turnipseed            ###")
+print("###                                               ###")
+print("#####################################################")
 
+selection = 0
+while (selection != 1 and selection != 2 and selection != 3):
+    print("Please select from the following options:\n\n")
+    print("1. User")
+    print("2. Bank Teller")
+    print("3. System Administrator\n\n")
+    selection = input("Please enter your selection:  ")
+    if (selection != 1 and selection != 2 and selection != 3):
+        print("\n\n#####################################################")
+        print("###                                               ###")
+        print("###         YOU ENTERED AN INVALID OPTION!        ###")
+        print("###                                               ###")
+        print("#####################################################\n\n")
 
-conn_db.close_connection(conn)
+print 'You selected %s' % selection
+
+##conn_db.close_connection(conn)
