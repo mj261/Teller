d7e900fc4f168ab6cf91bfac6ee27d851f45839d
Matthew Jones <matthewjones1170@gmail.com>
2015-04-01 17:40:15 -0500

    Database open and close (unencrypted)

---
+++ conn_db.py
@@ -0,0 +1,19 @@
+import mysql.connector
+from mysql.connector import Error
+
+def connect():
+    try:
+        conn = mysql.connector.connect(user='Teller', password='B@nkT311er',
+                              host='ec2-52-4-116-111.compute-1.amazonaws.com',
+                              database='BankTeller')
+        if conn.is_connected():
+            return conn
+
+    except Error as e:
+        print(e)
+
+
+def close_connection(conn):
+    conn.close()
+    if not conn.is_connected():
+        print ("Connection Closed")
--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 __author__ = 'Matthew Jones', 'Daniel Downs', 'Jennifer Jozefiak', 'Grant Richards', 'Ian Turnipseed'
 
-import mysql.connector
-from mysql.connector import Error
+import conn_db
 
-def connect():
-    try:
-        conn = mysql.connector.connect(user='Teller', password='B@nkT311er',
-                              host='ec2-52-4-116-111.compute-1.amazonaws.com',
-                              database='BankTeller')
-        if conn.is_connected():
-            print('Connected to MySQL database')
+conn = conn_db.connect()
 
-    except Error as e:
-        print(e)
 
-    finally:
-        conn.close()
 
-connect()
+conn_db.close_connection(conn)
