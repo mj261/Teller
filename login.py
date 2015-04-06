50a9fd6283b3e237aefd64333b0d07357b3cbfd9
Matthew Jones <matthewjones1170@gmail.com>
2015-04-06 14:07:24 -0500

    Admin Logins

--- login.py
+++ login.py
@@ -2,6 +2,7 @@
 """User, Admin, & Teller logins"""
 import conn_db
 from random import randint
+from passlib.hash import pbkdf2_sha256
 
 
 def teller_login(teller_number):
@@ -28,6 +29,12 @@ def create_user(username, password, email, name):
     return 1
 
 
+def encrypt_password(clear_password):
+    """Password Encryption"""
+    encrypted_password = pbkdf2_sha256.encrypt(clear_password, rounds=200000, salt_size=16)
+    return encrypted_password
+
+
 def user_login(username, password):
     """User Login"""
     print "User Logged in"
@@ -35,15 +42,29 @@ def user_login(username, password):
 
 def admin_login(username, password):
     admin_name = ''
+    password_hash = ''
     conn = conn_db.connect()
     cursor = conn.cursor()
-    query = """SELECT Name FROM Admins WHERE Username = '{0}' and Password = '{1}'""".format(username, password)
+    query = """SELECT Password FROM Admins WHERE Username = '{0}' LIMIT 1""".format(username)
     cursor.execute(query)
-    for name in cursor:
-        admin_name = name
-    conn_db.close_connection(conn)
-    admin_name = ''.join(map(str, admin_name))
-    return admin_name
+    for database_password in cursor:
+        password_hash = ''.join(map(str, database_password))
+    login = pbkdf2_sha256.verify(password, password_hash)
+    if not login:
+        cursor.close()
+        conn_db.close_connection(conn)
+        admin_name = ''
+        return admin_name
+    else:
+        conn = conn_db.connect()
+        cursor = conn.cursor()
+        query = """SELECT Name FROM Admins WHERE Username = '{0}' LIMIT 1""".format(username)
+        cursor.execute(query)
+        for admin_name in cursor:
+            admin_name = ''.join(map(str, admin_name))
+        cursor.close()
+        conn_db.close_connection(conn)
+        return admin_name
 
 
 def check_email(email):
@@ -75,7 +96,23 @@ def check_username(username):
     return present
 
 
+def check_admin_username(username):
+    """Check if username exists"""
+    present = 0
+    conn = conn_db.connect()
+    cursor = conn.cursor()
+    query = """SELECT EXISTS(SELECT * FROM Admins WHERE Username = '{0}') AS Does_Exist"""\
+        .format(username)
+    cursor.execute(query)
+    for does_exist in cursor:
+        present = does_exist
+    conn_db.close_connection(conn)
+    present = int(''.join(map(str, present)))
+    return present
+
+
 def create_new_teller(teller_name, admin_username):
+    """Create new teller"""
     teller_number = 0
     present = 1
     while present == 1:
@@ -102,3 +139,21 @@ def create_new_teller(teller_name, admin_username):
     cursor.execute(query)
     cursor.close()
     return teller_number
+
+
+def create_new_admin(admin_username, new_admin_username, admin_name, admin_email, admin_password):
+    """Create new admin"""
+    admin_password = encrypt_password(admin_password)
+    conn = conn_db.connect()
+    cursor = conn.cursor()
+    query = """INSERT INTO Admins VALUES ('{0}', '{1}', '{2}', '{3}')"""\
+        .format(admin_name, new_admin_username, admin_password, admin_email)
+    cursor.execute(query)
+    cursor.close()
+    operation = "Create new admin {0} with username {1}".format(admin_name, new_admin_username)
+    cursor = conn.cursor()
+    query = """INSERT INTO Admin_Log (User, Operation) VALUES ('{0}', '{1}')"""\
+        .format(admin_username, operation)
+    cursor.execute(query)
+    cursor.close()
+    return 1
