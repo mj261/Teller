e6c307052ba1da028b1343dfbe76c292fb6b3410
Jennifer Jozefiak <jmj347@msstate.edu>
2015-04-08 16:39:41 -0500

    User password change

--- SelectionScreens.py
+++ SelectionScreens.py
@@ -723,7 +723,20 @@ def user_home(username):
                 new_username =  raw_input("Change username to: ")
                 transactions.change_username(user_username, new_username)
         elif selection == 6:
-            pass
+            print "Please verify your username and password"
+            user_username = raw_input("Please enter your username:  ")
+            user_password = getpass.getpass("Please enter your password:  ")
+            login_result = login.user_login(user_username, user_password)
+            user_input = ""
+            if login_result == '':
+                while user_input != "y" and user_input != "n":
+                    user_input = raw_input("incorrect login information. Try Again? (y)es or (n)o:  ").lower()
+                    if user_input == "n":
+                        home_screen()
+                    elif user_input == "y":
+                        old_user()
+            else:
+                transactions.change_user_password(username)
         elif selection == 7:
             home_screen()
 
